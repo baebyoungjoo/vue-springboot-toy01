905bf3287987a379f065e5e6a19a86fa33ffe191
bjbae <bj.bae@usafe.co.kr>
2019-01-22 09:58:52 +0900

    Initial commit

---
+++ src/main/java/com/toy/board/VueSpringbootToy01Application.java
@@ -0,0 +1,14 @@
+package com.toy.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringbootToy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringbootToy01Application.class, args);
+	}
+
+}
+

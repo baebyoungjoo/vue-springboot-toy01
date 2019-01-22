8a8f41eba6171fab998699369eda938c2a09e9fb
bjbae <bj.bae@usafe.co.kr>
2019-01-22 16:46:08 +0900

    jasypt encrypt

--- src/main/java/com/toy/board/VueSpringbootToy01Application.java
+++ src/main/java/com/toy/board/VueSpringbootToy01Application.java
@@ -1,14 +1,35 @@
 package com.toy.board;
 
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class VueSpringbootToy01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VueSpringbootToy01Application.class, args);
 	}
-
+	
+    @Bean(name = "encryptorBean")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("password");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 }
 
--- src/test/java/com/toy/board/VueSpringbootToy01ApplicationTests.java
+++ src/test/java/com/toy/board/VueSpringbootToy01ApplicationTests.java
@@ -1,17 +1,42 @@
 package com.toy.board;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource("classpath:./application.properties")
 public class VueSpringbootToy01ApplicationTests {
 
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Value("${spring.datasource.driver-class-name}") private String DRIVER;
+	@Value("${spring.datasource.url}") private String URL;
+	@Value("${spring.datasource.username}") private String USERNAME;
+	@Value("${spring.datasource.password}") private String PASSWORD;
+	private String sql = "show databases";
+	
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		Class.forName(DRIVER);
+		
+		try (Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+			log.info("======================OK======================");
+			log.info(" " + con.toString() + " ");
+			log.info(" " + con.prepareStatement(sql).execute() + " ");
+			log.info("==============================================");
+		} catch(Exception e) {
+			log.debug(e.getMessage());
+		}
 	}
-
 }
 

486ff612e78e216865acabca001d8efa6b1c4d9b
bjbae <bj.bae@usafe.co.kr>
2019-02-21 18:06:13 +0900

    이용약관 틀
    이용약관 틀 및 내용 추가

--- frontend/src/components/joins/JoinTerms.vue
+++ frontend/src/components/joins/JoinTerms.vue
@@ -5,9 +5,13 @@
       <span class="ico_join ico_paging on"></span>
       <span class="ico_join ico_paging"></span>
     </div>
-    <span>약관동의</span> <br>
-    <input type="checkbox"/>
-    <span>내용</span>
+    
+    <div class="row justify-content-md-center terms_agreement">
+      <div class="col-md-3 title_agreement">약관 동의</div>
+      <div class="col-md-1 ml-auto right"><input type="checkbox"/></div>
+      <div class="w-100"></div>
+      <Terms/>
+    </div>
 
     <router-link class="btn btn-sm" to="/login">이전으로</router-link>
     <router-link class="btn btn-sm" to="/join/signUp">동의하기</router-link>
@@ -15,7 +19,15 @@
 </template>
 
 <script>
-  /* TODO */
+/* TODO */
+import Terms from '@/components/joins/Terms.vue'
+
+export default {
+  name: 'JoinTerms',
+  components: {
+    'Terms': Terms
+  }
+}
 </script>
 
 <style src="@/assets/styles/style.css">
